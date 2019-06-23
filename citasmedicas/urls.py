6b1d29248892b78bf5266b503a64ced72f4dd70d
Juan Sebastian <jlaguna27@itfip.edu.co>
2019-06-23 11:44:43 -0500

    Ajustando Settings

--- citasmedicas/urls.py
+++ citasmedicas/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from citasmedicas import settings
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView, LoginView
 from citas.views import Viendocitas, Insertarcita, Editcita, Elicita
